df48999c9542dbcf5064b3a1a1b9a907d7fd7796
rparker24 <robert@robertmparker.net>
2016-02-28 17:43:56 -0500

    add js code and images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,50 @@
+var targetNum = Math.floor(Math.random() * 101) + 19;
+var wins = 0;
+var losses = 0;
+var userScore = 0;
+var redValue = Math.floor(Math.random() * 12)
+var blueValue = Math.floor(Math.random() * 12);
+var yellowValue = Math.floor(Math.random() * 12);
+var greenValue = Math.floor(Math.random() * 12);
+
+
 $(document).ready(function() {
+
+	$('#randNum').append(targetNum);
 	
+	var numAdd = function() {
+		if (userScore == targetNum) {
+			$('#scoreCount').prepend("<p>You Won!</p>");
+			wins++;
+			$('#wins').append(wins);
+			userScore = 0;
+		} else if (userScore > targetNum) {
+			losses++;
+			$('#losses').append(losses);
+			userScore = 0;
+		}
+		$('#scoreDisplay').html(userScore);
+	};
+	// var numDisplay = $("<p>");
+
+	$('#red').click(function() {
+		userScore += redValue;
+		numAdd();
+	});
+	$('#blue').click(function() {
+		userScore += blueValue;
+		numAdd();
+	});
+	$('#yellow').click(function() {
+		userScore += yellowValue;
+		numAdd();
+	});
+	$('#green').click(function() {
+		userScore += greenValue;
+		numAdd();
+	});
+
+	$('#wins').append(wins);
+	$('#losses').append(losses);
+
 });
